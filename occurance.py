d6ef08dca825ca2b0cf44bd73a39ad210619b5d1
Jeyashree-200 <chokku1976_bcs29@mepcoeng.ac.in>
2026-03-30 11:03:11 +0530

    Create occurance.py

---
+++ occurance.py
@@ -0,0 +1,10 @@
+s=input("enter the string")
+visited=""
+for ch in s:
+    if ch not in visited:
+        count=0
+        for c in s:
+            if c==ch:
+                count+=1
+        print(ch,";",count)
+        visited+=ch
